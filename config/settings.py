4e6cc2941a45b40c00516ccccbf49cf1a1cd64d2
hossein <hossein.h.a770227@gmail.com>
2025-05-30 12:24:02 +0330

    add docker , add postgresql database

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'store'),  
+        'USER': os.environ.get('DATABASE_USER', 'root'),  
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'MYPASSWORD'), 
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),  
+        'PORT': int(os.environ.get('DATABASE_PORT', 5432)),  
+}
 }
 
 
