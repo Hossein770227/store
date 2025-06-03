81e779e83410b14c0eaa0b31a211c608107d3449
hossein <hossein.h.a770227@gmail.com>
2025-06-03 13:10:07 +0330

    add cart_add_view

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ app_name = 'cart'
 
 urlpatterns = [
     path('detail/', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.add_to_cart_view, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
+
+from store.models import Product
+from .forms import AddToCartProductForm
 from .cart import Cart
 
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html', context={'cart':cart})
+
+
+
+@require_POST
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
+
+    return redirect('cart:cart_detail')
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart.remove(product)
+
+    return redirect('cart:cart_detail')
