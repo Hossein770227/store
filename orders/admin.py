dc0337a520f37d13f4a7a2314f43c16d3ad6ef41
hossein <hossein.h.a770227@gmail.com>
2025-06-07 11:26:50 +0330

    add orderform , add orderadmin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, OrderItems
+
+
+class OrderItemsInLine(admin.TabularInline):
+    model = OrderItems
+    fields = ['product', 'quantity', 'price', ]
+    extra = 0
+
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'is_paid']
+    list_filter = ['user', 'first_name', 'last_name', 'is_paid']
+    search_fields = ['user', 'first_name', 'last_name',]
+    inlines = [OrderItemsInLine]
